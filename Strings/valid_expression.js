dd0728aa70c8877b20076c2b078d5cdfeeb155e2
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-08-19 23:03:32 -0700

    Sets

---
+++ Strings/valid_expression.js
@@ -0,0 +1,6 @@
+// Implement a function to check whether a given string is a valid mathematical
+//  expression (only digits, +,-,*,, and parentheses) and whether the
+//  parentheses are balanced.
+
+
+let expression=prompt("Enter a mathematical expression:");
