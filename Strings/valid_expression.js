5a6c2837568449d1c7bb622175ccf0668bb8675a
malaikaramzan52 <malaikaramzan52@gmail.com>
2025-08-22 00:39:30 -0700

    valid_expression

--- Strings/valid_expression.js
+++ Strings/valid_expression.js
@@ -1,6 +1,23 @@
-// Implement a function to check whether a given string is a valid mathematical
-//  expression (only digits, +,-,*,, and parentheses) and whether the
-//  parentheses are balanced.
+let expression = prompt("Enter a mathematical expression:");
 
+function isValidExpression(expression){
+    let balance = 0;
+    for(let char of expression){
+        if(!(char >= '0' && char <= '9' || char === '+' || char === '-' || char === '*' || char === '/' || char === '(' || char === ')')){
+            return false; // invalid character
+        }
+        // starting parentheses check
+        if (char === '(') balance++;
+        // closing parentheses check
+        else if (char === ')') {
+            balance--;
+            if (balance < 0) return false; // ) before (;
+        }
+    }
 
-let expression=prompt("Enter a mathematical expression:");
+    if (balance !== 0) return false; // unbalanced parentheses
+
+    return true; // valid
+}
+
+console.log(isValidExpression(expression));
